26efaf67c3c0691024dabdbf723be5e2a70450c9
Matt Diephouse <matt@diephouse.com>
2014-11-07 17:11:47 -0500

    Use Quick and Nimble

---
+++ SwiftGit2Tests/RepositorySpec.swift
@@ -0,0 +1,19 @@
+//
+//  RepositorySpec.swift
+//  RepositorySpec
+//
+//  Created by Matt Diephouse on 11/7/14.
+//  Copyright (c) 2014 GitHub, Inc. All rights reserved.
+//
+
+import SwiftGit2
+import Nimble
+import Quick
+
+class RepositorySpec: QuickSpec {
+	override func spec() {
+		it("example") {
+			expect(true).to(beTrue());
+		};
+	}
+}
