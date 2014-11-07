e6745d71641151d90b50a3496e9595ecef07f840
Matt Diephouse <matt@diephouse.com>
2014-11-07 17:03:13 -0500

    Add a Repository class

---
+++ SwiftGit2/Repository.swift
@@ -0,0 +1,14 @@
+//
+//  Repository.swift
+//  SwiftGit2
+//
+//  Created by Matt Diephouse on 11/7/14.
+//  Copyright (c) 2014 GitHub, Inc. All rights reserved.
+//
+
+import Foundation
+
+/// A git repository.
+class Repository {
+	
+}
