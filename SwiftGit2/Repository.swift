b5673e8620bb81fb1774f5e0168a46ee8f68a529
Matt Diephouse <matt@diephouse.com>
2014-11-17 00:31:33 -0500

    Actually load a repository

--- SwiftGit2/Repository.swift
+++ SwiftGit2/Repository.swift
@@ -7,8 +7,43 @@
 //
 
 import Foundation
+import LlamaKit
 
 /// A git repository.
-class Repository {
+public class Repository {
+	let git_repository: COpaquePointer
 	
+	/// Load the repository at the given URL.
+	///
+	/// URL - The URL of the repository.
+	///
+	/// Returns a `Result` with a `Repository` or an error.
+	class public func atURL(URL: NSURL) -> Result<Repository> {
+		let pointer = UnsafeMutablePointer<COpaquePointer>.alloc(1)
+		let result = git_repository_open(pointer, URL.fileSystemRepresentation)
+		
+		if result < GIT_OK.value {
+			pointer.dealloc(1)
+			return failure()
+		}
+		
+		let repository = Repository(git_repository: pointer.memory)
+		pointer.dealloc(1)
+		return success(repository)
+	}
+	
+	init(git_repository: COpaquePointer) {
+		self.git_repository = git_repository
+		
+		let path = git_repository_workdir(git_repository)
+		self.directoryURL = (path == nil ? nil : NSURL.fileURLWithPath(NSString(CString: path, encoding: NSUTF8StringEncoding)!, isDirectory: true))
+	}
+	
+	deinit {
+		git_repository_free(git_repository)
+	}
+	
+	/// The URL of the repository's working directory, or `nil` if the
+	/// repository is bare.
+	public let directoryURL: NSURL?
 }
--- SwiftGit2Tests/RepositorySpec.swift
+++ SwiftGit2Tests/RepositorySpec.swift
@@ -6,14 +6,24 @@
 //  Copyright (c) 2014 GitHub, Inc. All rights reserved.
 //
 
+import LlamaKit
 import SwiftGit2
 import Nimble
 import Quick
 
 class RepositorySpec: QuickSpec {
 	override func spec() {
-		it("example") {
-			expect(true).to(beTrue());
-		};
+		describe("+atURL()") {
+			it("should work if the repo exists") {
+				let repo = Fixtures.simpleRepository
+				expect(repo.directoryURL).notTo(beNil())
+			}
+			
+			it("should fail if the repo doesn't exist") {
+				let url = NSURL(fileURLWithPath: "blah")!
+				let result = Repository.atURL(url)
+				expect(result.error()).notTo(beNil())
+			}
+		}
 	}
 }
