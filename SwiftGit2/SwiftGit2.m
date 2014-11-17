01f1146b54ab832682cdb0da05f1ff192f0919ff
Matt Diephouse <matt@diephouse.com>
2014-11-16 19:11:06 -0500

    Initialize libgit2

---
+++ SwiftGit2/SwiftGit2.m
@@ -0,0 +1,15 @@
+//
+//  SwiftGit2.m
+//  SwiftGit2
+//
+//  Created by Matt Diephouse on 11/16/14.
+//  Copyright (c) 2014 GitHub, Inc. All rights reserved.
+//
+
+#import "SwiftGit2.h"
+
+
+__attribute__((constructor))
+static void SwiftGit2Init(void) {
+	git_threads_init();
+}
