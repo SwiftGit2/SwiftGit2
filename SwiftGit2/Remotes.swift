e40e19a3fc93d1619cf5dc541277f9ab15d40b00
Matt Diephouse <matt@diephouse.com>
2015-01-02 14:28:53 -0500

    Create files for remotes

---
+++ SwiftGit2/Remotes.swift
@@ -0,0 +1,8 @@
+//
+//  Remotes.swift
+//  SwiftGit2
+//
+//  Created by Matt Diephouse on 1/2/15.
+//  Copyright (c) 2015 GitHub, Inc. All rights reserved.
+//
+
---
+++ SwiftGit2Tests/RemotesSpec.swift
@@ -0,0 +1,12 @@
+//
+//  RemotesSpec.swift
+//  SwiftGit2
+//
+//  Created by Matt Diephouse on 1/2/15.
+//  Copyright (c) 2015 GitHub, Inc. All rights reserved.
+//
+
+import LlamaKit
+import SwiftGit2
+import Nimble
+import Quick
