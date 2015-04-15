9c7c53a97bbca152555ba68962bc46544a542296
Ben Chatelain <benchatelain@gmail.com>
2015-04-14 22:08:16 -0600

    Fix libcrypto linker errors
    Undefined symbols for architecture x86_64:
      "_opendir$INODE64", referenced from:
          _OPENSSL_DIR_read in libcrypto.a(o_dir.o)
      "_readdir$INODE64", referenced from:
          _OPENSSL_DIR_read in libcrypto.a(o_dir.o)
    ld: symbol(s) not found for architecture x86_64
    clang: error: linker command failed with exit code 1 (use -v to see invocation)

--- SwiftGit2/SwiftGit2.m
+++ SwiftGit2/SwiftGit2.m
@@ -8,8 +8,27 @@
 
 #import "SwiftGit2.h"
 
+#include <sys/dirent.h>
 
 __attribute__((constructor))
 static void SwiftGit2Init(void) {
 	git_libgit2_init();
+}
+
+// Declarations in iOS 8.3 /usr/include/dirent.h
+/*
+DIR *opendir(const char *) __DARWIN_ALIAS_I(opendir);
+struct dirent *readdir(DIR *) __DARWIN_INODE64(readdir);
+*/
+
+//http://stackoverflow.com/questions/29390112/libcrypto-a-symbols-not-found-for-architecture-i386#answer-29439324
+
+DIR * opendir$INODE64( char * dirName )
+{
+	return opendir( dirName );
+}
+
+struct dirent * readdir$INODE64( DIR * dir )
+{
+	return readdir( dir );
 }
