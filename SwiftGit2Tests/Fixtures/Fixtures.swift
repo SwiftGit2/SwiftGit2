6b02b0ba9cd2b698d613e11e4c1e70d19ba7138c
Ben Chatelain <benchatelain@gmail.com>
2015-04-22 22:50:13 -0600

    Stub out Fixtures for iOS tests
    iOS Tests still fail due to no unzipping and thus missing repos in temp dir

--- SwiftGit2Tests/Fixtures/Fixtures.swift
+++ SwiftGit2Tests/Fixtures/Fixtures.swift
@@ -32,8 +32,14 @@ final class Fixtures {
 	
 	func setUp() {
 		try! NSFileManager.defaultManager().createDirectoryAtURL(directoryURL, withIntermediateDirectories: true, attributes: nil)
-		
-		let bundle = NSBundle(identifier: "org.libgit2.SwiftGit2-OSXTests")!
+
+		#if os(OSX)
+			let platform = "OSX"
+		#else
+			let platform = "iOS"
+		#endif
+		let bundleIdentifier = String(format: "org.libgit2.SwiftGit2-%@Tests", arguments: [platform])
+		let bundle = NSBundle(identifier: bundleIdentifier)!
 		let zipURLs = bundle.URLsForResourcesWithExtension("zip", subdirectory: nil)! as [NSURL]
 		
 		for URL in zipURLs {
@@ -46,12 +52,20 @@ final class Fixtures {
 	}
 	
 	func unzipFileAtURL(fileURL: NSURL, intoDirectoryAtURL directoryURL: NSURL) {
-		let task = NSTask()
-		task.launchPath = "/usr/bin/unzip"
-		task.arguments = [ "-qq", "-d", directoryURL.path!, fileURL.path! ]
-		
-		task.launch()
-		task.waitUntilExit()
+        #if os(OSX)
+
+            let task = NSTask()
+            task.launchPath = "/usr/bin/unzip"
+            task.arguments = [ "-qq", "-d", directoryURL.path!, fileURL.path! ]
+            
+            task.launch()
+            task.waitUntilExit()
+
+        #else
+
+            assertionFailure("Tests not supported on iOS yet")
+
+        #endif
 	}
 	
 	// MARK: - Helpers
