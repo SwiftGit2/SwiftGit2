dc7ec07af98b03f44cf8574083c5c433e45457e3
Jake Van Alstyne <jakeva@gmail.com>
2017-01-23 20:00:43 -0700

    keep trying kiddo

--- SwiftGit2/Credentials.swift
+++ SwiftGit2/Credentials.swift
@@ -58,20 +58,20 @@ internal func credentialsCallback(cred: UnsafeMutablePointer<UnsafeMutablePointe
 //	}
 
 	// Find username_from_url
-	let name: String!
+	let name: String?
 	if username == nil {
 		name = nil
 	} else {
 		name = git_string_converter(username!)
 	}
 
-	print("user name : \(name)")
+	print("user name : \(name) \(name!)")
 
 	switch Credentials.fromPointer(payload!) {
 	case .Default():
 		result = git_cred_default_new(cred)
 	case .Agent():
-		result = git_cred_ssh_key_from_agent(cred, name)
+		result = git_cred_ssh_key_from_agent(cred, name!)
 	case .Plaintext(let password):
 		result = git_cred_userpass_plaintext_new(cred, name, password)
 	case .SSHMemory(let publicKey, let privateKey, let passphrase):
