4dd24cad977352419e80164b113affea3ec1647b
Arnon Keereena <arnon.keereena@allianz.com>
2017-05-01 17:19:05 +0200

    CommitIterator now not hold branch as reference. Make private var instead to prevent unexpected state change from external influence. Uses enum instead of tuple of stop and error to indicate state of Git command result.

--- SwiftGit2/CommitIterator.swift
+++ SwiftGit2/CommitIterator.swift
@@ -7,61 +7,68 @@ import Result
 import libgit2
 
 public class CommitIterator: IteratorProtocol {
-    public typealias Element = Result<Commit, NSError>
-    var repo: Repository
-    var branch: Branch
-    var revisionWalker: OpaquePointer? = nil
-    var oid: git_oid
-    private var unsafeCommit: OpaquePointer? = nil
-    
-    public init(repo: Repository, branch: Branch) {
-        self.repo = repo
-        self.branch = branch
-        self.oid = branch.oid.oid
-        setupRevisionWalker()
-    }
-    
-    deinit {
-        git_revwalk_free(self.revisionWalker)
-    }
-    
-    private func setupRevisionWalker() {
-        git_revwalk_new(&revisionWalker, repo.pointer)
-        git_revwalk_sorting(revisionWalker, GIT_SORT_TOPOLOGICAL.rawValue)
-        git_revwalk_sorting(revisionWalker, GIT_SORT_TIME.rawValue)
-        git_revwalk_push(revisionWalker, &oid)
-    }
-    
-    private func result(withName name: String, from result: Int32) -> (stop: Bool, error: NSError?) {
-        guard result == GIT_OK.rawValue else {
-            if result == GIT_ITEROVER.rawValue {
-                return (stop: true, error: nil)
-            } else {
-                return (stop: false, error: NSError(gitError: result, pointOfFailure: name))
-            }
-        }
-        return (stop: false, error: nil)
-    }
-    
-    public func next() -> Element? {
-        let revwalkGitResult = git_revwalk_next(&self.oid, self.revisionWalker)
-        let revwalkResult = result(withName: "git_revwalk_next", from: revwalkGitResult)
-        if revwalkResult.stop {
-            return nil
-        } else if let error = revwalkResult.error {
-            return Result.failure(error)
-        }
-        let lookupGitResult = git_commit_lookup(&self.unsafeCommit, self.repo.pointer, &self.oid)
-        let lookupResult = result(withName: "git_commit_lookup", from: lookupGitResult)
-        if lookupResult.stop {
-            return nil
-        } else if let error = lookupResult.error {
-            return Result.failure(error)
-        }
-        guard let commit = unsafeCommit else {
-            return nil
-        }
-        git_commit_free(unsafeCommit)
-        return Result.success(Commit(commit))
-    }
+	public typealias Element = Result<Commit, NSError>
+	let repo: Repository
+	private var oid: git_oid
+	private var revisionWalker: OpaquePointer? = nil
+
+	private enum Next {
+		case over
+		case ok
+		case error(NSError)
+
+		init(_ result: Int32, name: String) {
+			switch result {
+			case GIT_ITEROVER.rawValue:
+				self = .over
+			case GIT_OK.rawValue:
+				self = .ok
+			default:
+				self = .error(NSError(gitError: result, pointOfFailure: name))
+			}
+		}
+	}
+
+	init(repo: Repository, branch: Branch) {
+		self.repo = repo
+		self.oid = branch.oid.oid
+		setupRevisionWalker()
+	}
+
+	deinit {
+		git_revwalk_free(self.revisionWalker)
+	}
+
+	private func setupRevisionWalker() {
+		git_revwalk_new(&revisionWalker, repo.pointer)
+		git_revwalk_sorting(revisionWalker, GIT_SORT_TOPOLOGICAL.rawValue)
+		git_revwalk_sorting(revisionWalker, GIT_SORT_TIME.rawValue)
+		git_revwalk_push(revisionWalker, &oid)
+	}
+
+	private func next(withName name: String, from result: Int32) -> Next {
+		if result == GIT_OK.rawValue || result == GIT_ITEROVER.rawValue {
+			return Next(result, name: name)
+		} else {
+			return Next.error(NSError(gitError: result, pointOfFailure: name))
+		}
+	}
+
+	public func next() -> Element? {
+		var unsafeCommit: OpaquePointer? = nil
+		let revwalkGitResult = git_revwalk_next(&oid, revisionWalker)
+		let nextResult = next(withName: "git_revwalk_next", from: revwalkGitResult)
+		if case let .error(error) = nextResult {
+			return Result.failure(error)
+		} else if case .over = nextResult {
+			return nil
+		}
+		guard git_commit_lookup(&unsafeCommit, repo.pointer, &oid) == GIT_OK.rawValue,
+		      let unwrapCommit = unsafeCommit else {
+			return nil
+		}
+		let result: Element = Result.success(Commit(unwrapCommit))
+		git_commit_free(unsafeCommit)
+		return result
+	}
 }
