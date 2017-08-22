f422571d38fa591a40675cfe2f2b300c9d8cf260
Jake Van Alstyne 🎩 <jakeva@gmail.com>
2017-08-22 13:47:55 -0600

    fix whitespace indentation issue

--- SwiftGit2/Diffs.swift
+++ SwiftGit2/Diffs.swift
@@ -14,20 +14,20 @@ public struct GitDiffFile {
 }
 
 public enum GitStatus: Int {
-	case current								= 0
-	case indexNew								= 1
-	case indexModified					= 2
-	case indexDeleted						= 4
-	case indexRenamed						= 8
-	case indexTypeChange				= 16
-	case workTreeNew						= 32
-	case workTreeModified				= 64
-	case workTreeDeleted				= 128
-	case workTreeTypeChange			= 256
-	case workTreeRenamed				= 512
-	case workTreeUnreadable			= 1024
-	case ignored								= 2048
-	case conflicted							= 4096
+	case current                = 0
+	case indexNew               = 1
+	case indexModified          = 2
+	case indexDeleted           = 4
+	case indexRenamed           = 8
+	case indexTypeChange        = 16
+	case workTreeNew            = 32
+	case workTreeModified       = 64
+	case workTreeDeleted        = 128
+	case workTreeTypeChange     = 256
+	case workTreeRenamed        = 512
+	case workTreeUnreadable     = 1024
+	case ignored                = 2048
+	case conflicted             = 4096
 
 	public var value: UInt32 {
 		return UInt32(self.rawValue)
@@ -42,10 +42,10 @@ public struct GitDiffDelta {
 }
 
 public enum GitDiffFlag: Int {
-	case binary			= 0
-	case notBinary	= 1
-	case validId		= 2
-	case exists			= 4
+	case binary     = 0
+	case notBinary  = 1
+	case validId    = 2
+	case exists     = 4
 
 	public var value: UInt32 {
 		return UInt32(self.rawValue)
