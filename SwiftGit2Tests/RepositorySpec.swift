98bcc5b69c5a6a630a33c6693f7d1236df2f0354
Sean Antony <sja26@users.noreply.github.com>
2018-10-10 11:20:10 +0100

    Updated failing unit test

--- SwiftGit2Tests/RepositorySpec.swift
+++ SwiftGit2Tests/RepositorySpec.swift
@@ -24,7 +24,7 @@ class RepositorySpec: QuickSpec {
 				let url = URL(fileURLWithPath: "blah")
 				let result = Repository.at(url)
 				expect(result.error?.domain) == libGit2ErrorDomain
-				expect(result.error?.localizedDescription).to(match("Failed to resolve path"))
+				expect(result.error?.localizedDescription.lowercased()).to(beginWith("failed to resolve path"))
 			}
 		}
 
