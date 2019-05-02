aaf77d11bf91b3aaf8cdd9e1bc533e2ad2bb1b97
Loki <sergiy@vynnychenko.com>
2019-05-02 10:35:39 +0300

    Diffs WIP
    FindOptions OptionsSet added
    findSimilar method
    diffIndexToWorkDir method

--- SwiftGit2/Diffs.swift
+++ SwiftGit2/Diffs.swift
@@ -8,6 +8,8 @@
 
 import libgit2
 
+//git_diff_find_options
+
 public struct StatusEntry {
 	public var status: Diff.Status
 	public var headToIndex: Diff.Delta?
@@ -99,9 +101,51 @@ public struct Diff {
 		public static let validId    = Flags(rawValue: 1 << 1)
 		public static let exists     = Flags(rawValue: 1 << 2)
 	}
+	
+	public struct FindOptions: OptionSet {
+		// This appears to be necessary due to bug in Swift
+		// https://bugs.swift.org/browse/SR-3003
+		public init(rawValue: UInt32) {
+			self.rawValue = rawValue
+		}
+		public let rawValue: UInt32
+		
+		public static let byConfig						= FindOptions(rawValue: GIT_DIFF_FIND_BY_CONFIG.rawValue)
+		public static let renames						= FindOptions(rawValue: GIT_DIFF_FIND_RENAMES.rawValue)
+		public static let renamesFromRewrites			= FindOptions(rawValue: GIT_DIFF_FIND_RENAMES_FROM_REWRITES.rawValue)
+		public static let copies						= FindOptions(rawValue: GIT_DIFF_FIND_COPIES.rawValue)
+		public static let copiesFromUnmodified			= FindOptions(rawValue: GIT_DIFF_FIND_COPIES_FROM_UNMODIFIED.rawValue)
+		public static let rewrites						= FindOptions(rawValue: GIT_DIFF_FIND_REWRITES.rawValue)
+		public static let breakRewrites					= FindOptions(rawValue: GIT_DIFF_BREAK_REWRITES.rawValue)
+		public static let findAndBreakRewrites			= FindOptions(rawValue: GIT_DIFF_FIND_AND_BREAK_REWRITES.rawValue)
+		public static let forUntracked					= FindOptions(rawValue: GIT_DIFF_FIND_FOR_UNTRACKED.rawValue)
+		public static let all							= FindOptions(rawValue: GIT_DIFF_FIND_ALL.rawValue)
+		
+		public static let ignoreLeadingWhitespace		= FindOptions(rawValue: GIT_DIFF_FIND_IGNORE_LEADING_WHITESPACE.rawValue)
+		public static let ignoreWhitespace				= FindOptions(rawValue: GIT_DIFF_FIND_IGNORE_WHITESPACE.rawValue)
+		public static let dontIgnoreWhitespace			= FindOptions(rawValue: GIT_DIFF_FIND_DONT_IGNORE_WHITESPACE.rawValue)
+		public static let exactMatchOnly				= FindOptions(rawValue: GIT_DIFF_FIND_EXACT_MATCH_ONLY.rawValue)
+		public static let breakRewritesForRenamesOnly	= FindOptions(rawValue: GIT_DIFF_BREAK_REWRITES_FOR_RENAMES_ONLY.rawValue)
+		public static let removeUnmodified				= FindOptions(rawValue: GIT_DIFF_FIND_REMOVE_UNMODIFIED.rawValue)
+	}
+	
+	let pointer: OpaquePointer
+	
+	mutating public func findSimilar(options: FindOptions) {
+		var opt = git_diff_find_options(version: 1, flags: options.rawValue, rename_threshold: 50, rename_from_rewrite_threshold: 50, copy_threshold: 50, break_rewrite_threshold: 60, rename_limit: 200, metric: nil)
+		git_diff_find_similar(pointer, &opt)
+		
+		refreshDeltas()
+	}
 
 	/// Create an instance with a libgit2 `git_diff`.
 	public init(_ pointer: OpaquePointer) {
+		self.pointer = pointer
+		refreshDeltas()
+	}
+	
+	mutating private func refreshDeltas() {
+		deltas.removeAll()
 		for i in 0..<git_diff_num_deltas(pointer) {
 			if let delta = git_diff_get_delta(pointer, i) {
 				deltas.append(Diff.Delta(delta.pointee))
---
+++ SwiftGit2/RepositoryDiff.swift
@@ -0,0 +1,23 @@
+//
+//  RepositoryDiff.swift
+//  SwiftGit2
+//
+//  Created by Loki on 5/1/19.
+//  Copyright © 2019 GitHub, Inc. All rights reserved.
+//
+
+import Foundation
+import libgit2
+
+public extension Repository {
+	func diffIndexToWorkDir() -> Result<Diff,NSError> {
+		var diff: OpaquePointer? = nil
+		let result = git_diff_index_to_workdir(&diff, self.pointer, nil, nil)
+		
+		guard result == GIT_OK.rawValue else {
+			return Result.failure(NSError(gitError: result, pointOfFailure: "git_diff_index_to_workdir"))
+		}
+		
+		return .success(Diff(diff!))
+	}
+}
