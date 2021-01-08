19b929871abb0a013f5b683a3c024625167be3a5
Sergiy Vynnychenko <sergiy@vynnychenko.com>
2021-01-08 13:41:59 +0200

    func mapResult

--- SwiftGit2/Experimental/Ext/Array.swift
+++ SwiftGit2/Experimental/Ext/Array.swift
@@ -7,10 +7,25 @@
 
 
 public extension Array {
+	// [Result] -> Result<[], Error>
 	func aggregateResult<Value, Error>() -> Result<[Value], Error> where Element == Result<Value, Error> {
 		var values: [Value] = []
 		for result in self {
 			switch result {
+			case .success(let value):
+				values.append(value)
+			case .failure(let error):
+				return .failure(error)
+			}
+		}
+		return .success(values)
+	}
+	
+	// [value].mapResult { $0.methodWithResult() } -> Result<[transformedValue], Error>
+	func mapResult<Value, Error>(block: (Element) -> Result<Value, Error>) -> Result<[Value], Error> {
+		var values: [Value] = []
+		for item in self {
+			switch block(item) {
 			case .success(let value):
 				values.append(value)
 			case .failure(let error):
