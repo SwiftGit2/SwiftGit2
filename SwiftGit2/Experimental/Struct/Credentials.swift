942f7a815a57c4f51f45227def30a324ee78b7eb
UKS <ukushu@gmail.com>
2021-04-02 21:50:56 +0300

    Removed creds

--- SwiftGit2/Experimental/Struct/Credentials.swift
+++ SwiftGit2/Experimental/Struct/Credentials.swift
@@ -9,29 +9,26 @@
 import Foundation
 import Clibgit2
 
-public class Credentials {
-	var pointer : UnsafeMutablePointer<git_credential>? = UnsafeMutablePointer.allocate(capacity: 1)
-	
-	public init(username: String, publicKey: String, privateKey: String, passphrase: String) {
-		
-		pointer?.pointee = git_credential()
-		
-		
-		_ = username.withCString { user in
-			publicKey.withCString { public_key in
-				privateKey.withCString { private_key in
-					passphrase.withCString { pass in
-						git_credential_ssh_key_new(&pointer, user, public_key, private_key, pass)
-					}
-				}
-			}
-		}
-		
-		
-	}
-	
-	deinit {
-		git_credential_free(pointer)
-		pointer?.deallocate()
-	}
-}
+//public class Credentials {
+//	var pointer : UnsafeMutablePointer<git_credential>? = UnsafeMutablePointer.allocate(capacity: 1)
+//	
+//	public init(username: String, publicKey: String, privateKey: String, passphrase: String) {
+//		
+//		pointer?.pointee = git_credential()
+//		
+//		_ = username.withCString { user in
+//			publicKey.withCString { public_key in
+//				privateKey.withCString { private_key in
+//					passphrase.withCString { pass in
+//						git_credential_ssh_key_new(&pointer, user, public_key, private_key, pass)
+//					}
+//				}
+//			}
+//		}
+//	}
+//	
+//	deinit {
+//		git_credential_free(pointer)
+//		pointer?.deallocate()
+//	}
+//}
