31a0c464c21620f9a62fd98f2be8ad41798b6e15
UKS <skulptorrr@gmail.com>
2021-04-23 20:11:23 +0300

    StatusIterator fix

--- SwiftGit2/Experimental/Instance/Repository/Repository+Status.swift
+++ SwiftGit2/Experimental/Instance/Repository/Repository+Status.swift
@@ -8,15 +8,17 @@
 
 import Clibgit2
 
-public final class StatusIterator : InstanceProtocol {
-	public var pointer: OpaquePointer
+public final class StatusIterator {
+	public var pointer: OpaquePointer?
 	
-	public init(_ pointer: OpaquePointer) {
+	public init(_ pointer: OpaquePointer?) {
 		self.pointer = pointer
 	}
 	
 	deinit {
-		git_status_list_free(pointer)
+		if let pointer = pointer {
+			git_status_list_free(pointer)
+		}
 	}
 }
 
@@ -28,35 +30,50 @@ extension StatusIterator : RandomAccessCollection {
 
 	public subscript(position: Int) -> StatusEntry {
 		_read {
-			let s = git_status_byindex(pointer, position)
+			let s = git_status_byindex(pointer!, position)
 			yield StatusEntry(from: s!.pointee)
 		}
 	}
 	
 	public var startIndex	: Int { 0 }
-	public var endIndex		: Int { git_status_list_entrycount(pointer) }
+	public var endIndex		: Int {
+		if let pointer = pointer {
+			git_status_list_entrycount(pointer)
+		}
+		
+		return 0
+	}
 	
 	public func index(before i: Int) -> Int { return i - 1 }
 	public func index(after i: Int)  -> Int { return i + 1 }
 }
 
 public extension Repository {
+	
+	// CheckThatRepoIsEmpty
+	func repoIsBare() -> Bool {
+		let a = git_repository_is_bare(self.pointer)
+		
+		return a == 1 ? true : false
+	}
+	
+	
 	func status(options: StatusOptions = StatusOptions(), filter: String? = nil) -> Result<StatusIterator, NSError> {
 		var pointer: OpaquePointer? = nil
 		var git_options = options.git_options
-		
+
+		if repoIsBare() {
+			return .success( StatusIterator(nil) )
+		}
+
 		//TODO: Ugly hack. Need to move to StatusOptions() but it's a little bit difficult
 		if let filter = filter {
 			var dirPointer = UnsafeMutablePointer<Int8>(mutating: (filter as NSString).utf8String)
 			git_options.pathspec = git_strarray(strings: &dirPointer, count: 1)
 		}
-		
+
 		return _result( { StatusIterator(pointer!) }, pointOfFailure: "git_status_list_new") {
 			git_status_list_new(&pointer, self.pointer, &git_options)
 		}
-	}
-	
-	func statusCount(options: StatusOptions = StatusOptions(), filter: String? = nil) -> Result<Int, NSError> {
-		return status(options: options, filter: filter).map{ $0.endIndex}
 	}
 }
