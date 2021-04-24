9927d0260cbdf6f576a7dc4d54f8d85a1fe98854
Sergiy Vynnychenko <sergiy@vynnychenko.com>
2021-04-25 01:15:40 +0300

    TransferProgressCB

--- SwiftGit2/Experimental/Struct/Options/FetchOptions.swift
+++ SwiftGit2/Experimental/Struct/Options/FetchOptions.swift
@@ -9,12 +9,17 @@
 import Foundation
 import Clibgit2
 
+public typealias TransferProgressCB = (git_indexer_progress)->(Bool) // return false to cancel progree
+
 public struct FetchOptions {
 	var fetch_options = git_fetch_options()
 	let credentials: Credentials_OLD
 	
-	public init(credentials: Credentials_OLD = .default) {
+	var transferProgress : TransferProgressCB?
+	
+	public init(credentials: Credentials_OLD = .default, transferProgress: TransferProgressCB? = nil) {
 		self.credentials = credentials
+		self.transferProgress = transferProgress
 		setUp()
 	}
 		
@@ -24,6 +29,7 @@ public struct FetchOptions {
 		
 		fetch_options.callbacks.payload = self.toPointer()
 		fetch_options.callbacks.credentials = credentialsCallback
+		fetch_options.callbacks.transfer_progress = transferCallback
 	}
 	
 	internal static func fromPointer(_ pointer: UnsafeMutableRawPointer) -> FetchOptions {
@@ -65,4 +71,15 @@ internal func credentialsCallback(
 	}
 
 	return (result != GIT_OK.rawValue) ? -1 : 0
+}
+
+internal func transferCallback(stats: UnsafePointer<git_indexer_progress>?, payload: UnsafeMutableRawPointer? ) -> Int32 {
+	guard let stats = stats else { return -1 }
+	guard let payload = payload else { return -1 }
+	
+	// if progress callback didn't set just continue
+	guard let transferProgress = FetchOptions.fromPointer(payload).transferProgress else { return 0 }
+	
+	// if callback returns false return -1 to cancel transfer
+	return transferProgress(stats.pointee) ? 0 : -1
 }
