ead936da61e9c614cc2824ed1ce3076537dc4456
Sergiy Vynnychenko <sergiy@vynnychenko.com>
2021-05-27 00:30:33 +0300

    formatting

--- SwiftGit2Tests/MergeAnalysisTests.swift
+++ SwiftGit2Tests/MergeAnalysisTests.swift
@@ -1,10 +1,3 @@
-//
-//  AnalyzeMergeTests.swift
-//  SwiftGit2Tests
-//
-//  Created by loki on 24.05.2021.
-//  Copyright © 2021 GitHub, Inc. All rights reserved.
-//
 
 import XCTest
 @testable import SwiftGit2
@@ -12,20 +5,20 @@ import Essentials
 
 
 class MergeAnalysisTests: XCTestCase {
-
+    
     func testUpToDate() throws {
-		let info = PublicTestRepo()
-
-		guard let repo = Repository.clone(from: info.urlSsh, to: info.localPath)
-				.assertFailure("clone") else { fatalError() }
-		
-		repo.HEAD()
-			.flatMap { $0.asBranch() }
-			.flatMap { $0.upstream() }
-			.flatMap { $0.commitOID }
-			.flatMap { repo.annotatedCommit(oid: $0) }
-			.flatMap { repo.mergeAnalysis(their_head: $0) }
-			.assertEqual(to: .upToDate, "merge analysis")
+        let info = PublicTestRepo()
+        
+        guard let repo = Repository.clone(from: info.urlSsh, to: info.localPath)
+                .assertFailure("clone") else { fatalError() }
+        
+        repo.HEAD()
+            .flatMap { $0.asBranch() }
+            .flatMap { $0.upstream() }
+            .flatMap { $0.commitOID }
+            .flatMap { repo.annotatedCommit(oid: $0) }
+            .flatMap { repo.mergeAnalysis(their_head: $0) }
+            .assertEqual(to: .upToDate, "merge analysis")
     }
-
+    
 }
