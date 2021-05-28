9645e667a9603946cfc9d2998f7cd275f774252c
Sergiy Vynnychenko <sergiy@vynnychenko.com>
2021-05-28 21:35:17 +0300

    additional push tests

--- SwiftGit2Tests/RepositoryRemoteTests.swift
+++ SwiftGit2Tests/RepositoryRemoteTests.swift
@@ -84,6 +84,37 @@ class RepositoryRemoteTests: XCTestCase {
             .assertFailure("push")
     }
     
+    func testPushNoWriteAccess() {
+        let info = PublicTestRepo()
+        
+        // use HTTPS anonymous access
+        guard let repo = Repository.clone(from: info.urlHttps, to: info.localPath, options: CloneOptions(fetch: FetchOptions(auth: .auto)))
+                .assertFailure("clone") else { fatalError() }
+        
+        repo.t_commit(file: .fileA, with: .random, msg: "fileA random content")
+            .assertFailure("t_commit")
+        
+        // push should FAIL
+        repo.push()
+            .assertSuccess("push")
+    }
+    
+    func testPushNoRemote() {
+        let repo_ = GitTest.tmpURL
+            .flatMap { Repository.create(at: $0) }
+            .assertFailure("create repo")
+        
+        // for some reason it doesnt compile "let repo = repo"
+        guard let repo = repo_ else { fatalError() }
+        
+        repo.t_commit(file: .fileA, with: .random, msg: "fileA random content")
+            .assertFailure("t_commit")
+        
+        // push should FAIL
+        repo.push()
+            .assertSuccess("push")
+    }
+    
 }
 
 
