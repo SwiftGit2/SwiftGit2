9c2c4cae1744f129ea9644074d87e85f85e73c7a
Charlie Welsh <charlie@allotrope.dev>
2022-08-24 17:55:53 -0700

    Update package.resolved

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-		.package(path: "../Clibgit2")
+		.package(url: "https://github.com/allotropeinc/Clibgit2.git", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
