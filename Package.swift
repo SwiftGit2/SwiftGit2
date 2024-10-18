baa2b436389646a3edc27376b87c333b73af8f1b
Mathijs Bernson <mathijsb@q42.nl>
2024-10-18 11:10:29 +0200

    Add libgit2 as a git submodule

--- Package.swift
+++ Package.swift
@@ -13,7 +13,6 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/mbernson/libgit2.git", branch: "spm"),
         .package(url: "https://github.com/Quick/Quick.git", from: "7.0.0"),
         .package(url: "https://github.com/Quick/Nimble.git", from: "13.0.0"),
         .package(url: "https://github.com/ZipArchive/ZipArchive.git", from: "2.5.5"),
@@ -27,6 +26,95 @@ let package = Package(
             name: "SwiftGit2Tests",
             dependencies: ["SwiftGit2", "libgit2", "Quick", "Nimble", "ZipArchive"],
             resources: [.copy("Fixtures")]
+        ),
+        .target(
+            name: "libgit2",
+            path: "libgit2",
+            exclude: [
+                "deps/llhttp/CMakeLists.txt",
+                "deps/llhttp/LICENSE-MIT",
+                "deps/pcre/CMakeLists.txt",
+                "deps/pcre/COPYING",
+                "deps/pcre/LICENCE",
+                "deps/pcre/cmake",
+                "deps/pcre/config.h.in",
+                "deps/xdiff/CMakeLists.txt",
+                "deps/zlib/CMakeLists.txt",
+                "deps/zlib/LICENSE",
+                "src/libgit2/CMakeLists.txt",
+                "src/libgit2/experimental.h.in",
+                "src/libgit2/git2.rc",
+                "src/util/CMakeLists.txt",
+                "src/util/git2_features.h.in",
+                "src/util/hash/builtin.c",
+                "src/util/hash/builtin.h",
+                "src/util/hash/collisiondetect.c",
+                "src/util/hash/collisiondetect.h",
+                "src/util/hash/openssl.c",
+                "src/util/hash/openssl.h",
+                "src/util/hash/win32.c",
+                "src/util/hash/win32.h",
+                "src/util/win32",
+            ],
+            sources: [
+                "deps/llhttp",
+                "deps/pcre",
+                "deps/xdiff",
+                "deps/zlib",
+                "src/libgit2",
+                "src/util",
+            ],
+            publicHeadersPath: "include",
+            cSettings: [
+                .unsafeFlags([
+                  // Disable -fmodules flag. Clang finds (`struct entry`) in a different file (`search.h`).
+                  "-fno-modules",
+                  // Disable warning: "implicit conversion loses integer precision"
+                  "-Wno-single-bit-bitfield-constant-conversion",
+                  // Disable warning: "a function definition without a prototype is deprecated"
+                  "-Wno-deprecated-non-prototype",
+                ]),
+
+                .headerSearchPath("deps/llhttp"),
+                .headerSearchPath("deps/pcre"),
+                .headerSearchPath("deps/xdiff"),
+                .headerSearchPath("deps/zlib"),
+                .headerSearchPath("src/libgit2"),
+                .headerSearchPath("src/util"),
+
+                .define("LIBGIT2_NO_FEATURES_H"),
+                .define("GIT_ARCH_64", to: "1"),
+                .define("GIT_QSORT_BSD", to: "1"),
+                .define("GIT_IO_POLL", to: "1"),
+
+                // Git regex configuration
+                .define("GIT_REGEX_BUILTIN", to: "1"),
+                .define("PCRE_LINK_SIZE", to: "2"),
+                .define("SUPPORT_PCRE8", to: "1"),
+                .define("LINK_SIZE", to: "2"),
+                .define("PARENS_NEST_LIMIT", to: "250"),
+                .define("MATCH_LIMIT", to: "10000000"),
+                .define("MATCH_LIMIT_RECURSION", to: "10000000"),
+                .define("NEWLINE", to: "10"), // LF
+                .define("NO_RECURSE", to: "1"),
+                .define("POSIX_MALLOC_THRESHOLD", to: "10"),
+                .define("BSR_ANYCRLF", to: "0"),
+                .define("MAX_NAME_SIZE", to: "32"),
+                .define("MAX_NAME_COUNT", to: "10000"),
+
+                // Git SSH transport configuration
+                .define("GIT_SSH", to: "1"),
+                .define("GIT_SSH_EXEC", to: "1"),
+
+                // Git HTTPS transport configuration
+                .define("GIT_HTTPS", to: "1"),
+                .define("GIT_HTTPPARSER_BUILTIN", to: "1"),
+                .define("GIT_SECURE_TRANSPORT", to: "1"),
+
+                // Git cryptography configuration
+                .define("GIT_SHA1_COMMON_CRYPTO", to: "1"),
+                .define("GIT_SHA256_COMMON_CRYPTO", to: "1"),
+            ]
         ),
     ]
 )
